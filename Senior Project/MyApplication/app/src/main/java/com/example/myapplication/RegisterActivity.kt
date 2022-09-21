a64164ba36aae90716b17b6c54c3e9042255ea48
iyadhosn <80491883+iyadhosn@users.noreply.github.com>
2022-09-21 15:05:39 -0700

    test
    test

--- Senior Project/MyApplication/app/src/main/java/com/example/myapplication/RegisterActivity.kt
+++ Senior Project/MyApplication/app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -8,6 +8,5 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        Log.d("AAAAAAAAAAAAA", "AAAAAAAA")
-    }
+   }
 }
