6b24bdc8ca00e62c59327ab4524a14425055ea7f
Sophia Woo <48598362+minjisophia@users.noreply.github.com>
2022-09-28 09:42:24 -0700

    Base Settings UI, Connected to Dashboard Activity
    Created the base for settings ui, still need to add specific settings and features

--- Senior Project/MyApplication/app/src/main/java/com/example/myapplication/DashboardActivity.kt
+++ Senior Project/MyApplication/app/src/main/java/com/example/myapplication/DashboardActivity.kt
@@ -1,7 +1,11 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.preference.PreferenceManager
 import com.example.myapplication.databinding.ActivityDashboardBinding
 import com.example.myapplication.databinding.ActivityRegisterBinding
 
@@ -14,5 +18,32 @@ class DashboardActivity : AppCompatActivity() {
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //setContentView(R.layout.activity_dashboard)
+
+        mySettings()
+    }
+
+    private fun mySettings(){
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val signature = prefs.getString("signature", "")
+
+        //binding.apply {}
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.action_settings ->{
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
