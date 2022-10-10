8f754cb87ddff9aef5cba0b19cf9f31ed628a306
iyadhosn <80491883+iyadhosn@users.noreply.github.com>
2022-10-10 13:58:41 -0700

    Artwork Gui design
    Template for the artwork page.

--- Senior Project/MyApplication/app/src/main/java/com/example/myapplication/DashboardActivity.kt
+++ Senior Project/MyApplication/app/src/main/java/com/example/myapplication/DashboardActivity.kt
@@ -5,29 +5,59 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
 import androidx.preference.PreferenceManager
 import com.example.myapplication.databinding.ActivityDashboardBinding
-import com.example.myapplication.databinding.ActivityRegisterBinding
 
 
 class DashboardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDashboardBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //setContentView(R.layout.activity_dashboard)
-
         mySettings()
+        onClick()
+    }
+
+    private fun onClick() {
+        binding.collageBtn.setOnClickListener{
+            //binding.toolsLayout.visibility = View.GONE
+
+        }
+        binding.filterBtn.setOnClickListener {
+            //binding.toolsLayout.visibility = View.GONE
+        }
+        binding.brightnessBtn.setOnClickListener {
+            //binding.toolsLayout.visibility = View.GONE
+
+        }
+        binding.contrastBtn.setOnClickListener {
+            //binding.toolsLayout.visibility = View.GONE
+
+        }
+        binding.addTextBtn.setOnClickListener {
+            //binding.toolsLayout.visibility = View.GONE
+
+        }
+        binding.shapeBtn.setOnClickListener {
+            //binding.toolsLayout.visibility = View.GONE
+
+        }
+        binding.brushBtn.setOnClickListener {
+            //binding.toolsLayout.visibility = View.GONE
+
+        }
+
+
     }
 
     private fun mySettings(){
-
-        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-
-        val signature = prefs.getString("signature", "")
-
+        //val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        //val signature = prefs.getString("signature", "")
         //binding.apply {}
     }
 
@@ -37,7 +67,6 @@ class DashboardActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         when(item.itemId){
             R.id.action_settings ->{
                 val intent = Intent(this, SettingsActivity::class.java)
