0ec0eb31f58bd15271af06894b829f6cb4356cdc
frankalvarez013 <frankalvarez013@gmail.com>
2022-11-06 17:13:03 -0800

    Added the components to upload the images to the database

--- Senior Project/MyApplication/app/src/main/java/com/example/myapplication/User.kt
+++ Senior Project/MyApplication/app/src/main/java/com/example/myapplication/User.kt
@@ -1,10 +1,11 @@
 package com.example.myapplication
+import android.graphics.drawable.BitmapDrawable
 import java.util.LinkedList
 data class User(
     var name: String?,
     var email: String?,
     var password: String?,
-    //var images: LinkedList<String>
+    var images: LinkedList<BitmapDrawable>
 )
 
 
